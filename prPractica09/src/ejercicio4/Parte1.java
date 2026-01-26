4b55d8a30fc717743257edb52c0ef1bc84e79b82
1DAW <1DAW@DESKTOP-9DVBHID>
2026-01-26 14:54:08 +0100

    26/01/26

--- prPractica09/src/ejercicio4/Parte1.java
+++ prPractica09/src/ejercicio4/Parte1.java
@@ -9,6 +9,8 @@ public class Parte1 {
 	String cad = "Tiene la Tarara un vestido blanco que sólo se pone en el"+
 	"Jueves Santo";
 	
+	String cad2 = "Chupala Alberto";
+	
 		
 	//Averigua la longitud de la cadena. 
 	
@@ -21,13 +23,37 @@ public class Parte1 {
 	System.out.println("Carácter de la 2ª posición: " + cad.toCharArray()[1]);
 	
 	//Mostrar todas las vocales de una cadena. 
+	System.out.println("Vocales de la cadena: ");
+	char letraChar;
+	String letraString;
+	
+	for(int cont=0; cont<cad.length(); cont++) {
+		letraChar = cad.charAt(cont);
+		
+		letraString = Character.toString(letraChar);
+		
+		if(letraString.matches("[aAeEiIoOuU]")) {
+			System.out.print(letraChar + " ");
+		}
+		
+	}
+	
 	//Extraer la penúltima letra de una cadena. 
+	System.out.println("Penúltimo carácter: " + cad.charAt(cad.length()-2));
 	//Extrae y almacena en otro objeto del mismo tipo toda la cadena excepto los 2 primeros caracteres 
 	//(subcadena). 
+	System.out.println("Todos los caracteres menos los dos primeros: " + cad2.substring(2));
+	
+	
 	//Extrae y almacena en otro objeto del mismo tipo toda la cadena excepto los 2 primeros caracteres y 
 	//los dos últimos caracteres. 
+	System.out.println("Todos los caracteres menos los 2 primeros y los 2 últimos: " + cad2.substring(2, cad2.length()-2));
+	
 	//Modifica en la cadena anterior todas las veces que aparezca la vocal ‘a’ (minúscula) por  ‘A’ 
-	//(mayúscula). 
+	//(mayúscula).
+	
+	System.out.println("Sustituir la a minúscula por la A mayúscula: " + cad2.replace("a", "A"));
+	
 	}
 
 }
---
+++ prPractica09/src/ejercicio4/Parte2.java
@@ -0,0 +1,60 @@
+package ejercicio4;
+
+public class Parte2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String cad2;
+		String cad = "Tiene la Tarara un vestido blanco que sólo se pone en el"+
+				" Jueves Santo";
+				
+		
+//		• Crea un array de caracteres cuyos elementos serán los de la cadena anterior. 
+		char [] arrayCaracteres =cad.toCharArray();
+		
+		for (int cont = 0; cont<arrayCaracteres.length; cont++) {
+			System.out.print(arrayCaracteres[cont]);
+		}
+		System.out.println();
+		
+//		• Convierte la subcadena que creaste anteriormente a mayúscula, y que se quede modificada 
+//		internamente, es decir, no solo a la hora de imprimirla. 
+		cad2=cad.toUpperCase();
+		System.out.println(cad2);
+		
+//      Sacar la palabra "blanco" a un array con el método getChars y sybstring
+		try {
+			int longPalabra = "blanca".length();
+			
+		}
+		catch (StringIndexOutOfBoundsException e) {
+			System.out.println("Palabra no encontrada");
+			System.out.println(e.getMessage());
+		}
+//		• Comprueba si la cadena empieza por la palabra “Hola”. 
+		if(cad.startsWith("Hola") == false) {
+			System.out.println("No empieza por hola");
+		}
+		else {
+			System.out.println("Si empieza por hola");
+		}
+//		• Busca una subcadena que exista dentro de tu cadena, y otra que no exista (indexOf), recordad que 
+//		en la cadena original la letra ‘a’ se había cambiado a mayúscula. 
+		System.out.println(cad.indexOf("un vestido blanco"));
+		System.out.println(cad.indexOf("Polla"));
+		System.out.println(cad.contains("un vestido blanco")? "Si está" : "No está");
+		System.out.println(cad.contains("un vestido nigga")? "Si está" : "No está");
+//		• Busca todas las ocurrencias de una cadena. 
+		
+
+//		• Extrae de tu cadena la segunda palabra y almacénala en una nueva cadena. 
+//		• A partir de un array de caracteres crea un objeto de tipo cadena. 
+//		• Muestra el valor Unicode de cada uno de los caracteres de la cadena anterior (codePointAt). 
+//		• Crea un objeto de tipo StringBuffer, y prueba diferentes formas de concatenación con el método 
+//		Append. 
+//		• Insertar al objeto anterior una subcadena, a partir de la posición 7. 
+//		• A partir de un array de tipo byte que contiene números de un dígito, y con la ayuda del método 
+//		reverse de la clase StringBuffer, invierte el citado array.
+	}
+
+}
